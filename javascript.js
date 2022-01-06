ef70a1f6799320350bed22149706514dcf5afc39
lucasrc <lucasrczz01@gmail.com>
2022-01-06 17:10:03 -0300

    Finished to add all fetuares of the project!

--- javascript.js
+++ javascript.js
@@ -5,18 +5,23 @@ let cells = document.getElementsByClassName('cell');
 const reset = document.querySelector('#reset') 
 const eraser = document.querySelector('#eraser') 
 const rainbow = document.querySelector('#rainbow')
+const black = document.querySelector('#black')
 
 //Setamos o default da página para 16x16 e aplicamos o hoover
 chooseGrid(16)
 applyHover('black')
 
+black.addEventListener('click', function(){
+    applyHover('black')
+})
 rainbow.addEventListener('click', function(){
-    let arrRainbow = ['#7734EA','#00A7EA','#8AE800','#FAF100','#FFAA00',' #FF0061']
-    for(let i = 0 ; i<arrRainbow.length ; i++){
-    let randomColor = Math.floor(Math.random()*6);
-    let color = arrRainbow[randomColor];
-    console.log(color);
-    applyHover(color)}
+    let arrRainbow = ['#9400D3','#4B0082','#0000FF','#00FF00','#FFFF00',' #FF7F00','#FF0000']
+    cell= document.querySelectorAll('.cell') 
+    cell.forEach(function hoverEffect(cell){
+        cell.addEventListener('mouseover', function(e){ 
+        cell.style.backgroundColor = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+        });
+    })
 })
 eraser.addEventListener('click', function(){ 
     applyHover('white')
@@ -25,20 +30,20 @@ reset.addEventListener('click', function(){ // Botão de reset funcao de click
     empty(container) // chama a função para remover todos os filhos do container para um novo grid seja lancado
     anwser = prompt('Choose a new dimension for the grid(max:100):',)
     Number(anwser)
-    if(anwser>100){
+    while(anwser>100){
         empty(container)
         anwser = prompt('Enter a valid dimension:',)
         Number(anwser)   
     }
     chooseGrid(anwser) 
-    applyHover()
+    applyHover('black')
     
 })
 function applyHover(color){ //Com essa função reaplicamos o hover quando quisermos
-    cell= document.querySelectorAll('.cell') 
-    cell.forEach(function hoverEffect(cell){
+    cells= document.querySelectorAll('.cell') 
+    cells.forEach(function hoverEffect(cell){
         cell.addEventListener('mouseover', function(e){ 
-        cell.style.backgroundColor = ""+color
+        cell.style.backgroundColor = "" + color
         });
     })
 }
